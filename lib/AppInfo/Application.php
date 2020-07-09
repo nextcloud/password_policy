2ca03a6bc1b3ccf92347896bbe74f6bc47a1e6b2
Morris Jobke <hey@morrisjobke.de>
2020-07-09 22:25:38 +0200

    Drop legacy event
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -32,20 +32,17 @@ use OCA\Password_Policy\Listener\GenerateSecurePasswordEventListener;
 use OCA\Password_Policy\Listener\PasswordUpdatedEventListener;
 use OCA\Password_Policy\Listener\SuccesfullLoginListener;
 use OCA\Password_Policy\Listener\ValidatePasswordPolicyEventListener;
-use OCA\Password_Policy\PasswordValidator;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\Authentication\Events\LoginFailedEvent;
-use OCP\ILogger;
 use OCP\Security\Events\GenerateSecurePasswordEvent;
 use OCP\Security\Events\ValidatePasswordPolicyEvent;
 use OCP\User\Events\BeforePasswordUpdatedEvent;
 use OCP\User\Events\BeforeUserLoggedInEvent;
 use OCP\User\Events\PasswordUpdatedEvent;
 use OCP\User\Events\UserLoggedInEvent;
-use Symfony\Component\EventDispatcher\GenericEvent;
 
 class Application extends App implements IBootstrap {
 	public function __construct() {
@@ -65,21 +62,5 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function boot(IBootContext $context): void {
-		$server = $context->getServerContainer();
-
-		// TODO: remove this legacy event listener once https://github.com/nextcloud/user_sql/pull/146 is in
-		$symfonyDispatcher = $server->getEventDispatcher();
-		$symfonyDispatcher->addListener(
-			'OCP\PasswordPolicy::validate',
-			function (GenericEvent $event) use ($server) {
-				/** @var ILogger $logger */
-				$logger = $server->query(ILogger::class);
-				$logger->debug('OCP\PasswordPolicy::validate is deprecated. Listen to ' . ValidatePasswordPolicyEvent::class . ' instead');
-
-				/** @var PasswordValidator $validator */
-				$validator = $server->query(PasswordValidator::class);
-				$validator->validate($event->getSubject());
-			}
-		);
 	}
 }
