b248edec9808797a902e6f4cfeb144b923f91797
Joas Schilling <coding@schilljs.com>
2024-10-22 14:54:44 +0200

    fix(generation): Fix common-password check when we accidentally hit the same password length again
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/lib/Validator/CommonPasswordsValidatorTest.php
+++ tests/lib/Validator/CommonPasswordsValidatorTest.php
@@ -72,11 +72,15 @@ class CommonPasswordsValidatorTest extends TestCase {
 	}
 
 	public function dataValidate() {
-		return [
+		$attempts = [
 			['banana', false,  true],
 			['bananabananabananabanana', false,  true],
 			['banana',  true, false],
 			['bananabananabananabanana',  true,  true],
 		];
+		for ($i = 1; $i <= 39; $i++) {
+			$attempts[] = [str_repeat('$', $i), true, true];
+		}
+		return $attempts;
 	}
 }
