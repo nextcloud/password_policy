d82dd0b29552e420565e8ec675dd2f22ee5c2131
Joas Schilling <coding@schilljs.com>
2024-10-22 15:34:14 +0200

    build(passwords): Regenerate lists
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lists/list-32.php
+++ lists/list-32.php
@@ -5,4 +5,12 @@
  */
 return array (
   'williamfelipesantiago.santiago74' => true,
+  'princessthatguywhowantssomething' => true,
+  'peppergeorgefriedmancompaccounts' => true,
+  'passf.e.rg.u.sma.n.i.van.o.v.ich' => true,
+  'masterchubby_diago_girl123456789' => true,
+  'edik.grigorev.90edik.grigorev.90' => true,
+  'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' => true,
+  '33333333333333333333333333333333' => true,
+  '26067af27bb314ffbe8ba0528b907e31' => true,
 );
--- lists/list-37.php
+++ lists/list-37.php
@@ -6,4 +6,13 @@
 return array (
   'yankeesayka-ofrp3olgavaner8913gruscha' => true,
   'xc3xacntrxc3xackulxc3xacmbroqulxc3xac' => true,
+  'trustno1faweeeeeeeeeeeeedafdeeeeeeees' => true,
+  'roblespyreuyfufvfifhydjudes0315847654' => true,
+  'mnbvcxzasdfghjklpoiuytrewq12345678901' => true,
+  'lenakol123456789012345678901234567890' => true,
+  'iopqwe03xxwittingerwarsteinerzuxra231' => true,
+  'hammerboycongtubot_dathichgrilhaycuoi' => true,
+  'swedmciby4rwo5ozwkph6vhbew5ifd5dniqsb' => true,
+  '14081995376000923760009214081995thais' => true,
+  '0151139853366viniciusribeirodossantos' => true,
 );
